a8ec5cc74cd44a73b6b5a1f0f7f2d2290754c7b9
eRwinToRRes <96779958+erwinlh@users.noreply.github.com>
2025-05-12 00:24:18 -0400

    Actualizar crear_usuario_db_master.sql

--- models/crear_usuario_db_master.sql
+++ models/crear_usuario_db_master.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'flask_app_user'@'%' IDENTIFIED BY 'appPassword';
 
 -- Paso 2: Otorgar privilegios al usuario
-GRANT SELECT, INSERT, UPDATE, DELETE ON used_db.* TO 'flask_app_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON app_db.* TO 'flask_app_user'@'%';
 
 -- Paso 3 (Opcional): Refrescar los privilegios
 FLUSH PRIVILEGES;
